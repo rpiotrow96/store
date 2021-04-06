a99cae63065144d122754e66e9fbbfbd60b2d320
rpiotrow <rpiotrow@gmail.com>
2021-04-06 22:25:05 +0200

    dodano mozliwosc rejestracji

--- src/main/java/aitsi/store/controller/LoginController.java
+++ src/main/java/aitsi/store/controller/LoginController.java
@@ -1,11 +1,26 @@
 package aitsi.store.controller;
 
+import aitsi.store.entity.User;
+import aitsi.store.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
+
+    private UserService userService;
+
+    @Autowired
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -21,5 +36,35 @@ public class LoginController {
     @GetMapping("/logout")
     private String logout() {
         return "/welcome";
+    }
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String createNewUser(@Valid User user, BindingResult bindingResult, Model model) {
+        User userExists = userService.findByEmail(user.getEmail());
+
+        if(userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "Istnieje już w bazie użytkownik o takim emailu.");
+        }
+
+        if(bindingResult.hasErrors()) {
+            return "registration";
+        } else {
+            userService.saveUser(user);
+
+            model.addAttribute("successMessage", "Pomyślenie zarejestrowano użytkownika.");
+
+            return "login";
+        }
     }
 }
--- src/main/java/aitsi/store/repository/RoleRepository.java
+++ src/main/java/aitsi/store/repository/RoleRepository.java
@@ -1,2 +1,8 @@
-package aitsi.store.repository;public class RoleRepository {
+package aitsi.store.repository;
+
+import aitsi.store.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRole(String role);
 }
